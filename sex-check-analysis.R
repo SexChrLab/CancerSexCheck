cd3285fa50cad1dd9dcf45a9c96cc56b0fbd496c
Susan Massey <scmassey@asu.edu>
2023-11-28 16:29:05 -0700

    added inference functionality & cleaned up syntax

--- sex-check-analysis.R
+++ sex-check-analysis.R
@@ -5,10 +5,16 @@ library(tidyverse)
 library(ggplot2)
 library(plotly)
 
-# Read in data file(s) of interest/to use
-setwd('C:/Users/scmassey/Documents/CIREN/2023-Fall-MW/')
-counts = read.delim('data/TCGA_LIHC_TPM.tsv', row.names = 1) # for use from saved file
-# counts = data.frame(tpm_df, row.names = 1) # for testing - already in workspace
+# Read in data files of interest/to use
+setwd("C:/Users/scmassey/Documents/CIREN/2023-Fall-MW/")
+counts <- read.delim("data/TCGA_LIHC_TPM.tsv", row.names = 1)
+metadf <- read.delim("data/TCGA_LIHC_META.tsv", row.names = 1)
+
+# Modify row id's in metadf to correspond to similar format in counts
+meta_ids <- rownames(metadf)
+meta_ids <- gsub("[-]",".", meta_ids)
+rownames(metadf) <- meta_ids
+
 
 # Genes under consideration for inference:
 #   chromosome Y
@@ -23,37 +29,38 @@ counts = read.delim('data/TCGA_LIHC_TPM.tsv', row.names = 1) # for use from save
 #     UTY    ENSG00000183878
 #     ZFY    ENSG00000067646
 #     SRY    ENSG00000184895
-#     TSPY   (may be challenging due to multicopy) # LEFT OUT FOR NOW -
-#                                                  # NOT SURE WHICH ONE/S TO USE 
-#                                                  # ... AND THERE ARE 10+
-# 
+#     TSPY   (may be challenging due to multicopy, there are 10+, left out)
+#
 #   chromosome X
 #     XIST   ENSG00000229807
 #     AR     ENSG00000169083
 
-# make one each for reported males and one for reported females (with/without log transformation)
-# start with ALL together - curious if obvious bimodality
+# make one violin plot each for reported males and
+# one for reported females (with/without log transformation)
 
-ychr_genes = c('ENSG00000099721','ENSG00000067048','ENSG00000198692','ENSG00000012817',
-               'ENSG00000165246','ENSG00000099725','ENSG00000154620','ENSG00000114374',
-               'ENSG00000183878','ENSG00000067646','ENSG00000184895')
-ychr_gnames= c('AMELY','DDX3Y','EIF1AY','KDM5D','NLGN4Y','PRKY','TMSB4Y',
-               'USP9Y','UTY','ZFY','SRY')
+ychr_genes <- c("ENSG00000099721", "ENSG00000067048", "ENSG00000198692",
+                "ENSG00000012817", "ENSG00000165246", "ENSG00000099725",
+                "ENSG00000154620", "ENSG00000114374", "ENSG00000183878",
+                "ENSG00000067646", "ENSG00000184895")
+ychr_gnames <- c("AMELY", "DDX3Y", "EIF1AY", "KDM5D", "NLGN4Y", "PRKY",
+                 "TMSB4Y", "USP9Y", "UTY", "ZFY", "SRY")
 
-xchr_genes = c('ENSG00000229807','ENSG00000169083')
-Xchr_gnames= c('XIST','AR')
+xchr_genes  <- c("ENSG00000229807", "ENSG00000169083")
+xchr_gnames <- c("XIST", "AR")
 
-ychr_counts <- counts[ychr_genes,]
-xchr_counts <- counts[xchr_genes,]
+ychr_counts <- counts[ychr_genes, ]
+xchr_counts <- counts[xchr_genes, ]
 
 ## may want a version of new_counts for x and y?:
-new_counts1 <- data.frame(t(subset(xchr_counts, select = -c(gene_name,gene_type))))
+new_counts1 <- data.frame(t(subset(xchr_counts,
+                                   select = -c(gene_name, gene_type))))
 colnames(new_counts1) <- xchr_counts[, 1]
 
-new_counts2 <- data.frame(t(subset(ychr_counts, select = -c(gene_name,gene_type))))
+new_counts2 <- data.frame(t(subset(ychr_counts,
+                                   select = -c(gene_name, gene_type))))
 colnames(new_counts2) <- ychr_counts[, 1]
 
-new_counts <- data.frame(c(new_counts1,new_counts2))
+new_counts <- data.frame(c(new_counts1, new_counts2))
 row.names(new_counts) <- row.names(new_counts1)
 
 ################################################
@@ -64,147 +71,114 @@ row.names(new_counts) <- row.names(new_counts1)
 id_list <- rownames(new_counts)
 
 # Extract file and case uuid's from combo:
-f_uuid <- substr(id_list,2,37)
-c_uuid <- substr(id_list,39,74)
+splt_id_list <- strsplit(id_list, "[_]")
+c_uuid <- vector(mode = "character", length = length(id_list))
+f_uuid <- vector(mode = "character", length = length(id_list))
+
+for (j in 1:length(id_list)) {
+  f_uuid[j] <- splt_id_list[[j]][1]
+  c_uuid[j] <- splt_id_list[[j]][2]
+}
+# c_uuid <- unique(c_uuid)
+# 
+# old_f_uuid <- substr(id_list,2,37)
+# old_c_uuid <- substr(id_list,39,74)
 
 # Organize ids by case uuid (for viewing)
-id_sets <- split(id_list,c_uuid)
+id_sets <- split(id_list, c_uuid)
 
 repeats_df <- data.frame()
 rep_cases_list <- list()
 
-for (x in c_uuid){
-    if (length(id_sets[[x]])>1){
-      # whatdo
-      rows_keep <- id_sets[[x]]
+c_uuid <- unique(c_uuid)
 
-      # PUTTING THESE INTO A SEPARATE DF TO *LOOK* AT:
-      repeats_df <- bind_rows(repeats_df, new_counts[rows_keep[1],])
-      repeats_df <- bind_rows(repeats_df, new_counts[rows_keep[2],])
+for (x in c_uuid) {
+  if (length(id_sets[[x]]) > 1) {
+    # whatdo
+    rows_keep <- id_sets[[x]]
 
-      rep_cases_list <- c(rep_cases_list, substr(rows_keep[1],39,74))
-    }
+    # PUTTING THESE INTO A SEPARATE DF TO *LOOK* AT:
+    repeats_df <- bind_rows(repeats_df, new_counts[rows_keep[1], ])
+    repeats_df <- bind_rows(repeats_df, new_counts[rows_keep[2], ])
+
+    rep_cases_list <- c(rep_cases_list, substr(rows_keep[1], 39, 74))
+  }
 }
 
-# WRITE REPEATS DATAFRAME TO TSV FILE:
-write_tsv(
-  repeats_df,
-  "TCGA_LIHC_TPM_Repeated_Counts.tsv",
-  na = "NA",
-  append = FALSE,
-  col_names = TRUE,
-  quote = "none",
-  eol = "\n",
-  num_threads = readr_threads(),
-  progress = show_progress()
-)
+# # WRITE REPEATS DATAFRAME TO TSV FILE:
+# write_tsv(
+#   repeats_df,
+#   "TCGA_LIHC_TPM_Repeated_Counts.tsv",
+#   na = "NA",
+#   append = FALSE,
+#   col_names = TRUE,
+#   quote = "none",
+#   eol = "\n",
+#   num_threads = readr_threads(),
+#   progress = show_progress()
+# )
 ###################################################
 ###--END - CHECK FOR REPEATS AND LOOK AT VALUES-###
 ###################################################
 
 ### FINALLY, SEX CHECK OF THE SAMPLES!
 
-# Pseudo code to get started:
+sex_check <- data.frame(matrix(0, length(c_uuid), 2),
+                        row.names = c_uuid)
+colnames(sex_check) <- c("inferred", "assigned")
 
-# first, look at values of XIST and DDX3Y in each observation (by id)
-# and assign a category - assumed XX, assumed XY, or neither:
-
-for (id in ids){
-  if (new_counts[id,"XIST"] > 1.0 && new_counts[id,"DDX3Y"] < 1.0) {
-    #something to pair id with category "infer_XX"
-  } else if (new_counts[id,"XIST"] < 1.0 && new_counts[id,"DDX3Y"] > 1.0) {
-    #something to pair id with category "infer_XY"
+for (xid in c_uuid){
+  kid <- id_sets[[xid]]
+  if (all(new_counts[kid, "XIST"] > 1.0, 
+          new_counts[kid, "DDX3Y"] < 1.0,
+          new_counts[kid, "USP9Y"] < 1.0,
+          new_counts[kid, "UTY"] < 1.0,
+          new_counts[kid, "ZFY"] < 1.0)) {
+    sex_check[xid, "inferred"] <- "XX"
+  } else if (new_counts[kid, "XIST"] < 1.0 & any(new_counts[kid, "DDX3Y"] > 1.0,
+                                                 new_counts[kid, "USP9Y"] > 1.0,
+                                                 new_counts[kid, "UTY"] > 1.0,
+                                                 new_counts[kid, "ZFY"] > 1.0)) {
+    sex_check[xid, "inferred"] <- "XY"
   } else {
-    #something to pair id with category "no_inference"
+    sex_check[xid, "inferred"] <- "no_inference"
   }
+  sex_check[xid, "assigned"] <- metadf[xid, "mf_list"]
 }
-# note that using ids, we should be able to handle the double observations
-# of repeated samples, but might want to tread carefully there.
 
-# put that categorization in a new df with same combo uuids for the rownames
+# note that using ids, we should be able to handle the double observations of
+# repeated samples, esp with all/any, but might want to tread carefully there
+# >>> NOPE! GOT WARNINGS ABOUT THIS.. 
+# WILL NEED TO RETHINK HOW TO HANDLE THE MULTIPLE SAMPLE CASES HERE
 
-# then, take the ids x categories df and add M/F from corresponding metadata
-
-# lastly, can start doing fun things - plots,
-# comparing with survival/disease stage, etc.
+# AND NEXT, look at survival info and/or disease stage.
+# Q: add as additional columns to the sex_check df?
+#    or keep separate as e.g., "outcomes" and pair up by row ids?
+# A: look at what plot functions of interest expect as inputs and fit that
 
 
 ###################################################
 ###################################################
-# OLD PLOT CODE - WILL REVISE AFTER ISOLATING THE DATA WE WANT TO LOOK AT.
+# OLD PLOT CODE USED - REVISE AFTER ISOLATING THE DATA WE WANT TO LOOK AT.
 plotable_counts <- reshape(new_counts,
-               varying = Xchr_gnames,
-               v.names = 'counts',
-               timevar = "gene_names",
-               times = Xchr_gnames,
-               direction = 'long')
+                           varying = xchr_gnames,
+                           v.names = "counts",
+                           timevar = "gene_names",
+                           times = xchr_gnames,
+                           direction = 'long')
 
-# #vpy <- ggplot(ychr_counts)
-# #vpx <- ggplot(xchr_counts)
-# 
-# fig <- plot_ly(data = new_counts, x = ~XIST, y = ~AR)
-# fig
-# 
-# ## THis WORKED YESTERDAY 9/26 BUT ISN'T WORKING NOW - NOT SURE WHY...
-# fig <- new_counts %>%
-#   plot_ly(
-#     y = ~XIST,
-#     type = 'violin',
-#     box = list(
-#       visible = T
-#     ),
-#     meanline = list(
-#       visible = T
-#     ),
-#     x0 = 'XIST',
-#   )
-# 
-# fig <- fig %>%
-#   layout(
-#     yaxis = list(
-#       title = "",
-#       zeroline = F
-#     )
-#   )
-# 
-# fig
-# 
-# fig <- new_counts %>%
-#   plot_ly(
-#     y = ~AR,
-#     type = 'violin',
-#     box = list(
-#       visible = T
-#     ),
-#     meanline = list(
-#       visible = T
-#     ),
-#     x0 = 'AR',
-#   )
-# 
-# fig <- fig %>%
-#   layout(
-#     yaxis = list(
-#       title = "",
-#       zeroline = F
-#     )
-#   )
-# 
-# fig
-
-## THIS DOES WORK THOUGH! 
 fig <- plotable_counts %>%
   plot_ly(
     x = ~gene_names,
     y = ~counts,
-    type = 'violin',
+    type = "violin",
     box = list(
-      visible = T
+      visible = TRUE
     ),
     meanline = list(
-      visible = T
+      visible = TRUE
     ),
-    x0 = Xchr_gnames
+    x0 = xchr_gnames
   )
 
 fig <- fig %>%
@@ -214,39 +188,33 @@ fig <- fig %>%
     ),
     yaxis = list(
       title = "TPM",
-      zeroline = F
+      zeroline = FALSE
     )
   )
 
 fig
 
-
-# right now we mostly want to see the distribution of these to decide what's useful;
-# may also want a reference gene (or two, three..) that we expect to be unaffected
-
-
-
-
-new_counts2 <- data.frame(t(subset(ychr_counts, select = -c(gene_name,gene_type))))
+new_counts2 <- data.frame(t(subset(ychr_counts,
+                                   select = -c(gene_name, gene_type))))
 colnames(new_counts2) <- ychr_counts[, 1]
 
 plotable_counts2 <- reshape(new_counts2,
-                           varying = ychr_gnames,
-                           v.names = 'counts',
-                           timevar = "gene_names",
-                           times = ychr_gnames,
-                           direction = 'long')
+                            varying = ychr_gnames,
+                            v.names = "counts",
+                            timevar = "gene_names",
+                            times = ychr_gnames,
+                            direction = "long")
 
 fig <- plotable_counts2 %>%
   plot_ly(
     x = ~gene_names,
     y = ~counts,
-    type = 'violin',
+    type = "violin",
     box = list(
-      visible = T
+      visible = TRUE
     ),
     meanline = list(
-      visible = T
+      visible = TRUE
     ),
     x0 = ychr_gnames
   )
@@ -258,9 +226,8 @@ fig <- fig %>%
     ),
     yaxis = list(
       title = "TPM",
-      zeroline = F
+      zeroline = FALSE
     )
   )
 
 fig
-
